8526789551d7bbbb273725f755a0ffb23b7b6d90
NicandroFilho <nicandro.m@gmail.com>
2019-09-23 12:12:34 -0300

    About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,9 +1,18 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import sample.Main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,12 +38,32 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction(){
-        // TODO: 23/09/2019
-        System.out.println("onMenuItemAboutAction");
+        //load the View of About
+        loadView("/gui/About.fxml");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // TODO: 23/09/2019
+    }
 
+    private synchronized void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) (((ScrollPane)mainScene.getRoot()).getContent()); //get the main VBox
+
+            Node mainMenu = mainVBox.getChildren().get(0); //stores the firs children of main VBox. In this case, The Main Menu.
+            mainVBox.getChildren().clear(); //Clear all the Children in VBox
+            mainVBox.getChildren().add(mainMenu); //Add the main Menu back in the VBox, cleaned;
+            mainVBox.getChildren().addAll(newVBox.getChildren()); //Add all the Children of the new VBox in the Main VBox.
+
+
+
+        }catch (IOException e){
+            Alerts.showAlert("IO Exception", "Error loading View", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,31 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+    public static void setTextFiledInteger(TextField textField){
+        textField.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*")){
+                textField.setText(oldValue);
+            }
+        });
+    }
+
+    public static void setTextFieldMaxLength(TextField textField, int max){
+        textField.textProperty().addListener((obs, oldValue, newValue) -> {
+            if(newValue != null && newValue.length() > max ){
+                textField.setText(oldValue);
+            }
+        });
+    }
+
+    public static void setTextFiledDouble(TextField textField){
+        textField.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*([.]\\d*)?")){
+                textField.setText(oldValue);
+            }
+        });
+    }
+
+
+}
